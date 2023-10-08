f2c040f9e6222a1e2a8a978c51669aa2fdd8c572
Atanukumardey <atanudey2153@gmail.com>
2023-10-08 17:18:04 +0600

    env variable problem solved

--- database/connection.js
+++ database/connection.js
@@ -3,13 +3,13 @@ import Sequelize from 'sequelize';
 import config from './config/config.mjs';
 
 let sequelize;
-if (process.env.NODE_ENV === 'awsdbproduction') {
+if (process.env.TARGET_ENV === 'awsdbproduction') {
   sequelize = new Sequelize(config.awsdbproduction);
-} else if (process.env.NODE_ENV === 'production') {
+} else if (process.env.TARGET_ENV === 'production') {
   sequelize = new Sequelize(config.production);
-} else if (process.env.NODE_ENV === 'staging') {
+} else if (process.env.TARGET_ENV === 'staging') {
   sequelize = new Sequelize(config.staging);
-} else if (process.env.NODE_ENV === 'test') {
+} else if (process.env.TARGET_ENV === 'test') {
   sequelize = new Sequelize(config.test);
 } else {
   sequelize = new Sequelize(config.development);
